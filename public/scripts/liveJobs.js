09883785aa60f8a8409a377ebfe045f83e093cde
avneetvirdi <avneetvirdi26@gmail.com>
2024-08-18 15:00:06 -0400

    linejobs bug fixed

--- public/scripts/liveJobs.js
+++ public/scripts/liveJobs.js
@@ -31,41 +31,43 @@ console.log('Live Jobs');
 const allJobs = await fetchAllUsers('jobs');
 
 allJobs.forEach(function(job, idx) {
-    const html = `
-    <div class="col-3">
-        <div class="inner bg-white rounded p-3 position-relative">
-            <h1 class="position-absolute live-id"><b>${idx + 1}</b></h1>
-            <p>BAY: <strong>${job.bay}</strong></p>
-            <p>COMPANY: <strong>${job.company}</strong></p>
-            <p>UNIT: <strong>${job.unit}</strong></p>
-            <p>STATUS: <strong>${job.jobStatus}</strong></p>
-            <div style="overflow-x:scroll">
-                <p>Parts:</p>
-                <table class="border table tire-table">
-                    <tbody>
-                        <tr>
-                            <th>Qty</th>
-                            <th>Tire</th>
-                            <th>Desc.</th>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>   
-            <p>${job.notes}</p>
+    if(job.jobStatus != "completed" && job.jobStatus != "invoiced") {
+        const html = `
+        <div class="col-3">
+            <div class="inner bg-white rounded p-3 position-relative">
+                <h1 class="position-absolute live-id"><b>${idx + 1}</b></h1>
+                <p>BAY: <strong>${job.bay}</strong></p>
+                <p>COMPANY: <strong>${job.company}</strong></p>
+                <p>UNIT: <strong>${job.unit}</strong></p>
+                <p>STATUS: <strong>${job.jobStatus}</strong></p>
+                <div style="overflow-x:scroll">
+                    <p>Parts:</p>
+                    <table class="border table tire-table">
+                        <tbody>
+                            <tr>
+                                <th>Qty</th>
+                                <th>Tire</th>
+                                <th>Desc.</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>   
+                <p>${job.notes}</p>
+            </div>
         </div>
-    </div>
-    `;
-    document.querySelector(".live-jobs-container").insertAdjacentHTML('beforeend', html);
-    job.tire.forEach(function(tire) {
-        const newtire = `
+        `;
+        document.querySelector(".live-jobs-container").insertAdjacentHTML('beforeend', html);
+        job.tire.forEach(function(tire) {
+            const newtire = `
             <tr>
                 <td>${tire.qty}</td>
                 <td>${tire.name}</td>
                 <td>${tire.desc}</td>
             </tr>
         `;
-        document.querySelector('.tire-table').insertAdjacentHTML('beforeend', newtire);
-    });
+            document.querySelector('.tire-table').insertAdjacentHTML('beforeend', newtire);
+        });
+    }
 });
 
 
