174fad0832a0122c2d4395b8215c335e0c4e9ad1
avneetvirdi <avneetvirdi26@gmail.com>
2024-08-18 19:37:55 -0400

    vercel headers setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const path = require("path");
-const helmet = require("helmet");
+import express from "express";
+import path from "path";
+import helmet from "helmet";
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
